2c080a7c7c3bce6cdf6614d78d2db076890e212e
shairyl12 <shairylannbarrot@gmail.com>
2026-05-12 10:37:09 +0800

    Update crud_final_exam.js

--- crud_final_exam.js
+++ crud_final_exam.js
@@ -15,18 +15,13 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
-// DATABASE CONNECTION
 const db = mysql.createConnection({
-
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-
-    ssl: {
-        rejectUnauthorized: false
-    }
+    ssl: { rejectUnauthorized: false } // Aiven WILL reject the INSERT without this
 });
 
 
